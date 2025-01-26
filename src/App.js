ea4b05ced184dff9bb1eeceaa4b81cf84b11bf0f
Miku <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2025-01-26 07:55:09 +0100

    fix typo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          You are here to early, come back later!
+          You are here too early, come back later!
         </p>
         <a
           className="App-link"
