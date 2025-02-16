ec53d345afef616a80cb2994fba10b9e7fe6e41a
Miku <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2025-02-17 00:27:21 +0100

    vite init

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,80 @@
+import './LandingPage.css';
+import React, { useEffect } from 'react';
+
+
+function LandingPage() {
+
+    return (
+        <div className="LandingPage">
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="LandingPage__Name">
+                <div className='LandingPage__Name_First'>Konstantinos</div>
+                <div className='LandingPage__Name_Last'>‎ ‎ ‎ ‎ ‎ Katsimpas</div>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    );
+}
+
+export default LandingPage;
